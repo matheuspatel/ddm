316aacbce64f452a77a84fefc9d3c28a44e503d7
Matheus <matheuspatelamin@gmail.com>
2026-06-19 17:32:47 -0300

    Alt

--- app/src/main/java/com/example/Contador/MainActivity.java
+++ app/src/main/java/com/example/Contador/MainActivity.java
@@ -1,8 +1,14 @@
 package com.example.Contador;
-
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
 
 import androidx.appcompat.app.AppCompatActivity;
 
