a499d3270bdf3285e1b8989dde36bcdeec89c7c0
Matheus <matheuspatelamin@gmail.com>
2026-06-22 16:24:42 -0300

    Final

--- app/src/main/java/com/example/Contador/MainActivity.java
+++ app/src/main/java/com/example/Contador/MainActivity.java
@@ -4,30 +4,25 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-import androidx.appcompat.app.AppCompatActivity;
+    public class MainActivity extends AppCompatActivity {
 
-public class MainActivity extends AppCompatActivity {
+        int i = 0;
+        TextView textView;
 
-    int i=0;
-    TextView textView;
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
-        Button b=findViewById(R.id.button);
-        b.setOnClickListener (v -> {
-            i++;
-            textView.setText(Integer.toString(i));
-
-        });
+        @Override
+        protected void onCreate(Bundle savedInstanceState) {
+            super.onCreate(savedInstanceState);
+            setContentView(R.layout.activity_main);
+            Button b = findViewById(R.id.button);
+            textView = findViewById(R.id.contador);
+            b.setOnClickListener(v -> {
+                i++;
+                textView.setText(Integer.toString(i));
+            });
+        }
     }
-
-}
