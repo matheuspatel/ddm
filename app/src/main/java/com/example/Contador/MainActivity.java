541c153d916100dd169090be6a2cd1cf95204c1f
Matheus <matheuspatelamin@gmail.com>
2026-06-24 17:45:11 -0300

    Sorteio

--- app/src/main/java/com/example/Contador/MainActivity.java
+++ app/src/main/java/com/example/Contador/MainActivity.java
@@ -1,30 +1,52 @@
 package com.example.Contador;
+import android.app.AlertDialog;
 import android.os.Bundle;
-import android.util.Log;
+import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
+import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
-
+//pra funcionar o random aleatorio:
 import java.util.Random;
 
     public class MainActivity extends AppCompatActivity {
 
-        int i = 0;
-        TextView textView;
+        Button sorteio;
+        TextView result;
+        EditText n1;
+        EditText n2;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_main);
-            Button b = findViewById(R.id.button);
-            textView = findViewById(R.id.contador);
-            b.setOnClickListener(v -> {
-                i++;
-                textView.setText(Integer.toString(i));
-            });
+            EdgeToEdge.enable(this);
+
+            sorteio = findViewById(R.id.sorteio);
+            result = findViewById(R.id.result);
+            n1 = findViewById(R.id.n1);
+            n2 = findViewById(R.id.n2);
+
+            sorteio.setOnClickListener(this::fazSorteio);
+        }
+
+        private void fazSorteio(View view) {
+            try {
+                int nu1 = Integer.parseInt(n1.getText().toString());
+                int nu2 = Integer.parseInt(n2.getText().toString());
+
+                int res = new Random().nextInt((nu2 - nu1)+1)+nu1;
+                result.setText("O sorteio foi: " + res);
+
+            } catch (Exception e) {
+
+                new AlertDialog.Builder(this).setMessage("Digite 2 números para o sorteio:").show();
+
+            }
+
         }
     }
