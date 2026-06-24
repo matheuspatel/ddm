daf8223c10963a7c713b5619f62991338a2c9fba
Matheus <matheuspatelamin@gmail.com>
2026-06-24 15:45:04 -0300

    Contador

--- app/src/main/java/com/example/Contador/MainActivity.java
+++ app/src/main/java/com/example/Contador/MainActivity.java
@@ -4,12 +4,7 @@ import android.util.Log;
 import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
-
-import java.util.Random;
+import androidx.appcompat.app.AppCompatActivity;git
 
     public class MainActivity extends AppCompatActivity {
 
