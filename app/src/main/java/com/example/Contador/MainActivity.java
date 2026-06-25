37dc24c6710054aebc92360f01dded29859653f5
Matheus <matheuspatelamin@gmail.com>
2026-06-25 16:28:48 -0300

    IMC Calculadora

--- app/src/main/java/com/example/Contador/MainActivity.java
+++ app/src/main/java/com/example/Contador/MainActivity.java
@@ -1,17 +1,23 @@
 package com.example.Contador;
 import android.os.Bundle;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.ImageView;
+import android.widget.TextView;
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
 
     public class MainActivity extends AppCompatActivity {
 
-        Button bProx, bBack;
-        ImageView imgV;
-        int imagens [] = new int []{R.drawable.mia,R.drawable.messi,R.drawable.vini,R.drawable.bozo,R.drawable.davi,R.drawable.kratos,R.drawable.dorao};
-
-        int posicao=0;
+        Button bCalcular;
+        ImageView imgResult;
+        EditText peso;
+        EditText altura;
+        TextView result;
+        TextView classf;
+        double vPeso;
+        double vAltura;
+        double IMC;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
@@ -19,33 +25,52 @@ import androidx.appcompat.app.AppCompatActivity;
             EdgeToEdge.enable(this);
             setContentView(R.layout.activity_main);
 
-            bProx = findViewById(R.id.btProx);
-            bBack = findViewById(R.id.btBack);
-            imgV = findViewById(R.id.imgVi);
+            imgResult = findViewById(R.id.imgResultado);
+            imgResult.setImageResource(R.drawable.perfil);
+            bCalcular = findViewById(R.id.buCalcular);
+            peso = findViewById(R.id.peso);
+            altura = findViewById(R.id.altura);
+            result = findViewById(R.id.resultado);
+            classf = findViewById(R.id.classf);
 
-            bProx.setOnClickListener( v -> {
-                posicao++;
-                if (posicao>= imagens.length) {
-                    posicao=0;
+            bCalcular.setOnClickListener(v -> {
+//pra tranformar pro tipo double decimal
+                vPeso = Double.parseDouble(peso.getText().toString());
+                vAltura = Double.parseDouble(altura.getText().toString());
+//formula propria do imc
+                IMC = vPeso / (vAltura * vAltura);
+//molda pra 2 casas, o set smp define o texto
+                result.setText(String.format("IMC: %.1f", IMC));
+
+                if (IMC < 18.5) {
+                    classf.setText("Abaixo do peso");
+                    imgResult.setImageResource(R.drawable.abaixopeso);
+                    return;
                 }
-                imgV.setImageResource(imagens[posicao]);
-        });
-
-            bBack.setOnClickListener( v -> {
-                posicao--;
-                if (posicao < 0) {
-                    posicao = imagens.length - 1;
+                if (IMC <= 24.9) {
+                    classf.setText("Peso normal");
+                    imgResult.setImageResource(R.drawable.normal);
+                    return;
                 }
-                imgV.setImageResource(imagens[posicao]);
-
+                if (IMC <= 29.9) {
+                    classf.setText("Sobrepeso");
+                    imgResult.setImageResource(R.drawable.sobrepeso);
+                    return;
+                }
+                if (IMC <= 34.9) {
+                    classf.setText("Obesidade grau 1");
+                    imgResult.setImageResource(R.drawable.obesidade1);
+                    return;
+                }
+                if (IMC <= 39.9) {
+                    classf.setText("Obesidade grau 2");
+                    imgResult.setImageResource(R.drawable.obesidade2);
+                    return;
+                }
+                //agr nn vai precisar do if pq ja teve os returns, so sobra isso
+                classf.setText("Obesidade grau 3");
+                imgResult.setImageResource(R.drawable.obesidade3);
             });
 
-        }
-
-        @Override
-        protected void onResume() {
-            super.onResume();
-            posicao = 0;
-            imgV.setImageResource(imagens[posicao]);
         }
     }
