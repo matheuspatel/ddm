b0dc893a94ed7159da1295f41d7bee7633222e42
Matheus <matheuspatelamin@gmail.com>
2026-06-25 08:54:33 -0300

    Sorteio

--- app/src/main/java/com/example/Contador/MainActivity.java
+++ app/src/main/java/com/example/Contador/MainActivity.java
@@ -1,16 +1,12 @@
 package com.example.Contador;
-import android.app.AlertDialog;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
-import android.widget.Toast;
 import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.core.graphics.Insets;
-import androidx.core.view.ViewCompat;
-import androidx.core.view.WindowInsetsCompat;
 //pra funcionar o random aleatorio:
 import java.util.Random;
 
@@ -25,28 +21,37 @@ import java.util.Random;
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             EdgeToEdge.enable(this);
+            setContentView(R.layout.activity_main);
 
             sorteio = findViewById(R.id.sorteio);
             result = findViewById(R.id.result);
             n1 = findViewById(R.id.n1);
             n2 = findViewById(R.id.n2);
 
-            sorteio.setOnClickListener(this::fazSorteio);
+            sorteio.setOnClickListener(this::fazSort);
         }
 
-        private void fazSorteio(View view) {
-            try {
+        private void fazSort(View view) {
+            if (n1.getText().toString().isEmpty() ||
+                    n2.getText().toString().isEmpty()) {
+
+                new AlertDialog.Builder(this)
+                        .setMessage("Digite 2 números para o sorteio")
+                        .show();
+
+                return;
+            }
+
                 int nu1 = Integer.parseInt(n1.getText().toString());
                 int nu2 = Integer.parseInt(n2.getText().toString());
 
                 int res = new Random().nextInt((nu2 - nu1)+1)+nu1;
                 result.setText("O sorteio foi: " + res);
 
-            } catch (Exception e) {
-
-                new AlertDialog.Builder(this).setMessage("Digite 2 números para o sorteio:").show();
-
+             /* poderíamos usar o try e o : catch (Exception e) {
+                result.setText("ERRO");
+                isso pra verificar erros como se tentarem colocar letras, mas como nos testes ja tá puxando apenas numero, estamos usando o if pra verificar e usar o dialog
+*/
             }
 
         }
-    }
