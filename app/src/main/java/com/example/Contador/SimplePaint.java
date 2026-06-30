2976ac1f225f3fc2202f2f0f8699c54f57bfc1dd
Matheus <matheuspatelamin@gmail.com>
2026-06-30 20:35:53 -0300

    Simple Paint Finalizado

--- app/src/main/java/com/example/Contador/SimplePaint.java
+++ app/src/main/java/com/example/Contador/SimplePaint.java
@@ -5,6 +5,7 @@ import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
 import android.graphics.Path;
+import android.graphics.drawable.ColorDrawable;
 import android.util.AttributeSet;
 import android.view.MotionEvent;
 import android.view.View;
@@ -21,33 +22,40 @@ public class SimplePaint extends View {
     List<Path> mPathList;
     Paint currentPaint;
     Path currentPath;
+    ColorDrawable currentColor;
 
     public SimplePaint(Context context, @Nullable AttributeSet attrs) {
         super(context, attrs);
         mPaintList = new ArrayList<Paint>();
         mPathList = new ArrayList<Path>();
 
-//config do paint
-        currentPaint.setColor(Color.RED);
+// inicializar a variavel
+        currentColor = new ColorDrawable();
+        currentColor.setColor(Color.BLACK);
+
+        // pra chamar o metodo/função
+        inicializaCamadaDesenho();
 
     }
-//metodo de inicialização pra evitar aquela questao de estilo mudar por conta da config do path e paint ser nova, e garantir inicialização coerente:
+//definindo o metodo de inicialização pra evitar aquela questao de estilo mudar por conta da config do path e paint ser nova, e garantir inicialização coerente:
     public void inicializaCamadaDesenho(){
         currentPaint = new Paint();
         currentPath = new Path();
         //config do paint
         currentPaint.setStyle(Paint.Style.STROKE);
         currentPaint.setStrokeWidth(20);
+        //config pra pegar o inteiro do currentcolor e colocar no paint
+        currentPaint.setColor(currentColor.getColor());
     }
 
     @Override
     protected void onDraw(@NonNull Canvas canvas) {
         super.onDraw(canvas);
         for (int i = 0 ; i < mPaintList.size(); i++){
-            //renderizar o canvas, desenhar na tela
+            //renderizar a lista do canvas, desenhar na tela:
             canvas.drawPath(mPathList.get(i), mPaintList.get(i));
         }
-        //se nn usar esse aqui que tem os elementos usados, fica td errado e estranho:
+        //se nn usar esse aqui que tem os elementos usados, fica td errado e estranho. Usando ele mosra a camada atual
         canvas.drawPath(currentPath, currentPaint);
     }
 
@@ -69,9 +77,8 @@ public class SimplePaint extends View {
                 currentPath.lineTo(lx, ly);
                 mPaintList.add(currentPaint);
                 mPathList.add(currentPath);
-                //reinicializar as variaveis:
-                currentPaint = new Paint();
-                currentPath = new Path();
+                //usando no lugar de reinicializar as variaveis:
+                inicializaCamadaDesenho();
 
                 break;
             default:
@@ -82,6 +89,7 @@ public class SimplePaint extends View {
     }
 
     public void setColor(Color color) {
+        currentColor.setColor(color.toArgb());
         currentPaint.setColor(color.toArgb());
     }
 }
