0da446aefad52eef25be2261925c2e02e62e8bc9
Matheus <matheuspatelamin@gmail.com>
2026-07-01 17:39:18 -0300

    SQL no Android

--- app/src/main/java/com/example/Contador/MainActivity.java
+++ app/src/main/java/com/example/Contador/MainActivity.java
@@ -1,25 +1,30 @@
 package com.example.Contador;
+
 import android.os.Bundle;
-import android.util.Log;
-import android.widget.Button;
-import android.widget.TextView;
-import android.widget.Toast;
-import androidx.appcompat.app.AppCompatActivity;git
+import androidx.appcompat.app.AppCompatActivity;
+import android.database.sqlite.SQLiteDatabase;
 
     public class MainActivity extends AppCompatActivity {
-
-        int i = 0;
-        TextView textView;
+        //declarando a variável, e é privada pra ser segura apenas nessa classe:
+        private SQLiteDatabase banco;
 
         @Override
         protected void onCreate(Bundle savedInstanceState) {
             super.onCreate(savedInstanceState);
             setContentView(R.layout.activity_main);
-            Button b = findViewById(R.id.button);
-            textView = findViewById(R.id.contador);
-            b.setOnClickListener(v -> {
-                i++;
-                textView.setText(Integer.toString(i));
-            });
+//executando o metodo que cria ou abre um banco de dados no sqlite com o nome banco
+            //o metodo ta no contexto da activity, e o banco é aberto com o parametro modo privado, só uma activity acessa por vez
+            //o this faz com que seja obrigado a estar na activity que tem acesso ao contexto do sistema pra acessar
+            banco = openOrCreateDatabase("banco", getBaseContext().MODE_PRIVATE, null);
+            //criar a tabela
+            banco.execSQL("CREATE TABLE IF NOT EXISTS alunos (_id INTEGER PRIMARY KEY AUTOINCREMENT, nome_UC TEXT NOT NULL, nota DECIMAL NOT NULL)");
+
+        }
+        //metodos inserir nota e show
+        public void insereNota(){
+
+        }
+        public void showAlunos(){
+
         }
     }
